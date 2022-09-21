e6eab44112e95930e72da3522eb121e34e87b68f
boaventuravh <111531346+boaventuravh@users.noreply.github.com>
2022-09-21 08:55:33 -0300

    Primeira parte da lista concluída

---
+++ 01Lista/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001Lista
+{
+    class Program
+    {
+        static void Main()
+        {
+            Rgb rep = new Rgb();
+            rep.SetRed(11); // valor de rep.red
+            rep.SetGreen(10); // valor de rep.green
+            rep.SetBlue(25); // valor de rep.blue
+
+            Rgb repDois = new Rgb();        
+            repDois.SetRed(37); // valor de repDois.red
+            repDois.SetGreen(150); // valor de repDois.green
+            repDois.SetBlue(190); // valor de repDois.blue
+            
+            //double luminosidade = rep.CalcularLuminosidade(rep.red, rep.green, rep.blue);
+            //Console.WriteLine("A luminosidade para essa composicao de cores eh: " + luminosidade);
+
+            double lumiDois = repDois.CalcularLuminosidade(repDois.red, repDois.green, repDois.blue);
+            Console.WriteLine("A luminosidade para essa composicao de cores eh (repDois): " + lumiDois);
+            
+            Rgb copia = new Rgb(rep);
+            Console.WriteLine("Copia das cores: " + copia.red + ", " + copia.green + " e " + copia.blue +".");
+
+            bool comparar;
+
+            comparar = rep. ComparaCoresDiferente(rep, copia);
+
+            Console.WriteLine("Valor da comparação: " + comparar);
+            
+            // os 3 Get's abaixo são exclusivamente leitura
+            int vermelho = rep.GetRed(rep);
+            int verde = rep.GetGreen(rep);
+            int azul = rep.GetBlue(rep);
+
+            //Console.WriteLine("red: " + vermelho +" green: " + verde + " blue: " + azul);
+
+            Rgb equiCinza = new Rgb();
+            equiCinza.EquivalenteCinza(repDois, equiCinza);
+            Console.WriteLine("Equivalente cinza de repDois red: " + equiCinza.red + " green: " + equiCinza.green + " blue: " + equiCinza.blue);
+            double lumiCinza = equiCinza.CalcularLuminosidade(equiCinza.red, equiCinza.green, equiCinza.blue);
+            Console.WriteLine("Luminosidade equivalente cinza: " + lumiCinza);
+
+            string hexa;
+            hexa = repDois.RepreHexaDois(repDois);
+            Console.WriteLine("representacao hexadecimal de repDois: " + hexa);
+            hexa = repDois.RepreHexaDois(rep);
+            Console.WriteLine("representacao hexadecimal de rep: " + hexa);
+
+            Rgb clara = new Rgb();
+            
+            clara.SetRed(100); 
+            clara.SetGreen(100); 
+            clara.SetBlue(100);
+            Console.WriteLine("Antes do clareamento, red: "+clara.red+", green: "+clara.green+" e blue: "+clara.blue);
+
+            clara.Clarear(clara, 0.17);
+            Console.WriteLine("Depois do clareamento, red: " + clara.red + ", green: " + clara.green + " e blue: " + clara.blue);
+
+            Rgb escura = new Rgb();
+
+            escura.SetRed(100);
+            escura.SetGreen(100);
+            escura.SetBlue(100);
+            Console.WriteLine("Antes do escurecimento, red: " + escura.red + ", green: " + escura.green + " e blue: " + escura.blue);
+
+            escura.Escurecer(escura, 0.1);
+            Console.WriteLine("Depois do escurecimento, red: " + escura.red + ", green: " + escura.green + " e blue: " + escura.blue);
+
+            Rgb testInstancia = new Rgb();
+            testInstancia = testInstancia.Instanciar(rep);
+            Console.WriteLine("Instancia, red: " + testInstancia.red + ", green: " + testInstancia.green + " e blue: " + testInstancia.blue);
+
+            Rgb preSelect = new Rgb();
+            preSelect.SelecionarCor(preSelect, "preta");
+            Console.WriteLine("Canal R: " + preSelect.red + " canal G: " + preSelect.green + " canal B: " + preSelect.blue);
+        }
+
+        
+    }
+}
---
+++ 01Lista/Rgb.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections;
+
+public class Rgb //essa classe como um todo é o item i da lista
+{
+
+	public int red, green, blue;
+
+	// os três métodos abaixo são o número iii da lista, juntamente com os métodos GetRed, GetGreen e GetBlue
+	public void SetRed(int red)
+	{
+		this.red = red;
+	}
+
+	public void SetGreen(int green)
+	{
+		this.green = green;
+	}
+
+	public void SetBlue(int blue)
+	{
+		this.blue = blue;
+	}
+
+	// o método abaixo é o item iv da lista de exercícios.  Atende ao item x também?
+	public double CalcularLuminosidade(double red, double green, double blue)
+	{
+		double luminosidade = Math.Truncate((red * 0.3) + (green * 0.59) + (blue * 0.11));
+		return luminosidade;
+	}
+
+	/*os dois "public Rgb" abaixo são um exemplo de sobrecargas de construtores
+	 item v da lista de exercícios*/
+
+	public Rgb() { } // esse construtor atende a solicitação do item vii
+
+	public Rgb(int red, int green, int blue) // esse construtor atende a solicitação do item viii
+	{
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+	}
+
+
+
+	// o construtor abaixo é referente ao item vi
+
+	public Rgb(Rgb corOriginal)
+	{
+		this.red = corOriginal.red;
+		this.green = corOriginal.green;
+		this.blue = corOriginal.blue;
+	}
+
+	// o método abaixo é referente ao item ix 
+
+	public bool ComparaCores(Rgb cor1, Rgb cor2)
+	{
+		if (cor1.red == cor2.red && cor1.green == cor2.green && cor1.blue == cor2.blue)
+			return true;
+		else
+			return false;
+	}
+
+	public bool ComparaCoresDiferente(Rgb cor1, Rgb cor2)
+	{
+		if (cor1.red != cor2.red)
+			return false;
+
+		if (cor1.green != cor2.green)
+			return false;
+
+		if (cor1.blue != cor2.blue)
+			return false;
+
+		return true;
+	}
+
+	// as 3 propriedades abaixo são referentes ao item x
+	public int GetRed(Rgb cor)
+	{
+		return cor.red;
+	}
+
+	public int GetGreen(Rgb cor)
+	{
+		return cor.green;
+	}
+
+	public int GetBlue(Rgb cor)
+	{
+		return cor.blue;
+	}
+
+	// o método abaixo é referente ao item  xi
+	public void EquivalenteCinza(Rgb cor, Rgb equivalenteCinza)
+	{
+		double dobRed, dobGreen, dobBlue;
+		dobRed = cor.red;
+		dobGreen = cor.green;
+		dobBlue = cor.blue;
+		double luminosidade = Math.Truncate(dobRed * 0.3 + dobGreen * 0.59 + dobBlue * 0.11);
+		equivalenteCinza.red = (int)luminosidade; //MathTruncate
+		equivalenteCinza.green = (int)luminosidade;
+		equivalenteCinza.blue = (int)luminosidade;
+	}
+
+	// os dois métodos abaixo são referentes ao item xii, duas formas diferentes de reslver o problema
+	public string RepreHexa(Rgb cor)
+	{
+		int res, quo;
+
+		string redDig1, redDig2;
+		quo = cor.red / 16;
+		res = cor.red % 16;
+		switch (quo)
+		{
+			case 10: redDig1 = "A"; break;
+			case 11: redDig1 = "B"; break;
+			case 12: redDig1 = "C"; break;
+			case 13: redDig1 = "D"; break;
+			case 14: redDig1 = "E"; break;
+			case 15: redDig1 = "F"; break;
+			default: redDig1 = quo.ToString(); break;
+		}
+		switch (res)
+		{
+			case 10: redDig2 = "A"; break;
+			case 11: redDig2 = "B"; break;
+			case 12: redDig2 = "C"; break;
+			case 13: redDig2 = "D"; break;
+			case 14: redDig2 = "E"; break;
+			case 15: redDig2 = "F"; break;
+			default: redDig2 = res.ToString(); break;
+		}
+
+		string greenDig1, greenDig2;
+		quo = cor.green / 16;
+		res = cor.green % 16;
+		switch (quo)
+		{
+			case 10: greenDig1 = "A"; break;
+			case 11: greenDig1 = "B"; break;
+			case 12: greenDig1 = "C"; break;
+			case 13: greenDig1 = "D"; break;
+			case 14: greenDig1 = "E"; break;
+			case 15: greenDig1 = "F"; break;
+			default: greenDig1 = quo.ToString(); break;
+		}
+		switch (res)
+		{
+			case 10: greenDig2 = "A"; break;
+			case 11: greenDig2 = "B"; break;
+			case 12: greenDig2 = "C"; break;
+			case 13: greenDig2 = "D"; break;
+			case 14: greenDig2 = "E"; break;
+			case 15: greenDig2 = "F"; break;
+			default: greenDig2 = res.ToString(); break;
+		}
+
+		string blueDig1, blueDig2;
+		quo = cor.blue / 16;
+		res = cor.blue % 16;
+		switch (quo)
+		{
+			case 10: blueDig1 = "A"; break;
+			case 11: blueDig1 = "B"; break;
+			case 12: blueDig1 = "C"; break;
+			case 13: blueDig1 = "D"; break;
+			case 14: blueDig1 = "E"; break;
+			case 15: blueDig1 = "F"; break;
+			default: blueDig1 = quo.ToString(); break;
+		}
+		switch (res)
+		{
+			case 10: blueDig2 = "A"; break;
+			case 11: blueDig2 = "B"; break;
+			case 12: blueDig2 = "C"; break;
+			case 13: blueDig2 = "D"; break;
+			case 14: blueDig2 = "E"; break;
+			case 15: blueDig2 = "F"; break;
+			default: blueDig2 = res.ToString(); break;
+		}
+
+		string repHexa = "#" + redDig1 + redDig2 + greenDig1 + greenDig2 + blueDig1 + blueDig2;
+		return repHexa;
+	}
+
+	public string RepreHexaDois(Rgb cor)
+	{
+		string redDig1, redDig2;
+		redDig1 = (cor.red / 16).ToString();
+		Convert.ToString(Convert.ToInt32(redDig1, 10), 16);
+		switch (redDig1)
+		{
+			case "10": redDig1 = "A"; break;
+			case "11": redDig1 = "B"; break;
+			case "12": redDig1 = "C"; break;
+			case "13": redDig1 = "D"; break;
+			case "14": redDig1 = "E"; break;
+			case "15": redDig1 = "F"; break;
+		}
+		redDig2 = (cor.red % 16).ToString();
+		Convert.ToString(Convert.ToInt32(redDig2, 10), 16);
+		switch (redDig2)
+		{
+			case "10": redDig2 = "A"; break;
+			case "11": redDig2 = "B"; break;
+			case "12": redDig2 = "C"; break;
+			case "13": redDig2 = "D"; break;
+			case "14": redDig2 = "E"; break;
+			case "15": redDig2 = "F"; break;
+		}
+
+		string greenDig1, greenDig2;
+		greenDig1 = (cor.green / 16).ToString();
+		Convert.ToString(Convert.ToInt32(greenDig1, 10), 16);
+		switch (greenDig1)
+		{
+			case "10": greenDig1 = "A"; break;
+			case "11": greenDig1 = "B"; break;
+			case "12": greenDig1 = "C"; break;
+			case "13": greenDig1 = "D"; break;
+			case "14": greenDig1 = "E"; break;
+			case "15": greenDig1 = "F"; break;
+		}
+		greenDig2 = (cor.green % 16).ToString();
+		Convert.ToString(Convert.ToInt32(greenDig2, 10), 16);
+		switch (greenDig2)
+		{
+			case "10": greenDig2 = "A"; break;
+			case "11": greenDig2 = "B"; break;
+			case "12": greenDig2 = "C"; break;
+			case "13": greenDig2 = "D"; break;
+			case "14": greenDig2 = "E"; break;
+			case "15": greenDig2 = "F"; break;
+		}
+
+		string blueDig1, blueDig2;
+		blueDig1 = (cor.blue / 16).ToString();
+		Convert.ToString(Convert.ToInt32(blueDig1, 10), 16);
+		switch (blueDig1)
+		{
+			case "10": blueDig1 = "A"; break;
+			case "11": blueDig1 = "B"; break;
+			case "12": blueDig1 = "C"; break;
+			case "13": blueDig1 = "D"; break;
+			case "14": blueDig1 = "E"; break;
+			case "15": blueDig1 = "F"; break;
+		}
+		blueDig2 = (cor.blue % 16).ToString();
+		Convert.ToString(Convert.ToInt32(blueDig2, 10), 16);
+		switch (blueDig2)
+		{
+			case "10": blueDig2 = "A"; break;
+			case "11": blueDig2 = "B"; break;
+			case "12": blueDig2 = "C"; break;
+			case "13": blueDig2 = "D"; break;
+			case "14": blueDig2 = "E"; break;
+			case "15": blueDig2 = "F"; break;
+		}
+
+		string repHexa = "#" + redDig1 + redDig2 + greenDig1 + greenDig2 + blueDig1 + blueDig2;
+		return repHexa;
+	}
+
+	//o método abaixo é referente ao item xiii 
+	public void Clarear(Rgb cor, double perc)
+	{
+		double dobRed, dobGreen, dobBlue;
+		dobRed = cor.red + cor.red * perc;
+		dobGreen = cor.green + cor.green * perc;
+		dobBlue = cor.blue + cor.blue * perc;
+		cor.red = (int)dobRed;
+		cor.green = (int)dobGreen;
+		cor.blue = (int)dobBlue;
+	}
+
+	//o método abaixo é referente ao item xiv
+	public void Escurecer(Rgb cor, double perc)
+	{
+		double dobRed, dobGreen, dobBlue;
+		dobRed = cor.red - cor.red * perc;
+		dobGreen = cor.green - cor.green * perc;
+		dobBlue = cor.blue - cor.blue * perc;
+		cor.red = (int)dobRed;
+		cor.green = (int)dobGreen;
+		cor.blue = (int)dobBlue;
+	}
+
+	//o método abaixo é referente ao item xv
+	public Rgb Instanciar(Rgb cor)
+	{
+		Rgb corNova = new Rgb();
+		corNova.red = cor.red;
+		corNova.green = cor.green;
+		corNova.blue = cor.blue;
+		return corNova;
+	}
+
+	//os atributos e o método abaixo são refentes ao item xvi
+	public int[] preta = new int[3] {0,0,0};
+	public int[] branca = new int[3] { 255, 255, 255 };
+	public int[] vermelha = new int[3] { 255, 0, 0 };
+	public int[] verde = new int[3] { 0, 255, 0 };
+	public int[] azul = new int[3] { 0, 0, 255 };
+
+	public void SelecionarCor(Rgb cor, string escolha)
+    {
+		if(escolha == "preta")
+        {
+			cor.red = preta[0];
+			cor.green = preta[1];
+			cor.blue = preta[2];
+        }
+		else if (escolha == "branca")
+		{
+			cor.red = branca[0];
+			cor.green = branca[1];
+			cor.blue = branca[2];
+		}
+		else if (escolha == "vermelha")
+		{
+			cor.red = vermelha[0];
+			cor.green = vermelha[1];
+			cor.blue = vermelha[2];
+		}
+		else if (escolha == "verde")
+		{
+			cor.red = verde[0];
+			cor.green = verde[1];
+			cor.blue = verde[2];
+		}
+		else if (escolha == "azul")
+		{
+			cor.red = azul[0];
+			cor.green = azul[1];
+			cor.blue = azul[2];
+		}
+	}
+}

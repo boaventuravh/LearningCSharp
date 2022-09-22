5bc88a5926824c0dd3004beeafb6ae76879e087f
boaventuravh <111531346+boaventuravh@users.noreply.github.com>
2022-09-22 11:54:29 -0300

    Segunda parte da lista de exercícios
    Iniciada, ainda verificando como fazer o que foi solicitado

---
+++ 02Lista/02Lista/Imagem.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Imagem
+{
+    //int larg, 
+    public int [,,] imagem = new int[,,] { { 0 }, { 1 }, {2,3,4} };
+    
+}
---
+++ 02Lista/02Lista/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Lista
+{
+    class Program
+    {
+        static void Main()
+        {
+            Imagem oi = new Imagem();
+            Console.WriteLine(oi.imagem);
+        }
+        
+    }
+}

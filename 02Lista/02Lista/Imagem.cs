ed1b8fbc3aa4c092da5569901941949cccd21ebf
boaventuravh <111531346+boaventuravh@users.noreply.github.com>
2022-09-23 11:54:51 -0300

    Avanços na segunda parte da lista
    Já tive uma ideia de como representar a imagem. No entanto, só consegui fazer o item i com MÉTODO, e a solicitação é que seja feito com CONSTRUTOR.

--- 02Lista/02Lista/Imagem.cs
+++ 02Lista/02Lista/Imagem.cs
@@ -2,7 +2,75 @@
 
 public class Imagem
 {
-    //int larg, 
-    public int [,,] imagem = new int[,,] { { 0 }, { 1 }, {2,3,4} };
+    //atributos
+    public int largura, altura, tamanho;
+    public int red, green, blue;
+    public int[] rgb = new int[3];
+
+    //métodos
+    public void SetLargura (int largura)
+    {
+        this.largura = largura;
+    }
+    public void SetAltura(int altura)
+    {
+        this.altura = altura;
+    }
+    public void SetRed(int red)
+    {
+        this.red = red;
+    }
+
+    public void SetGreen(int green)
+    {
+        this.green = green;
+    }
+
+    public void SetBlue(int blue)
+    {
+        this.blue = blue;
+    }
+
+    public void SetRgb()
+    {
+        this.rgb[0] = this.red;
+        this.rgb[1] = this.green;
+        this.rgb[2] = this.blue;
+    }
+    public int CalculaTamanho()
+    {
+        this.tamanho = this.altura * this.largura;
+        return this.tamanho;
+    }
+
+   /* public int[,,] FazImagem(int tamanho)
+    {
+        tamanho = this.tamanho;
+        int largura = tamanho / this.altura;
+        int altura = tamanho / this.largura;
+        int[,,] pixel = new int[altura, largura, this.rgb.Length];
+        for (int alt = 0; alt < altura; alt++)
+            for (int larg = 0; larg < largura; larg++)
+                for (int r = 0; r < this.rgb.Length; r++)
+                    this.rgb[r] = 255;
+        return pixel;
+    }*/
+
+    //construtores
+    public Imagem() { }
+    public Imagem(int tamanho) // todos os pixels ajustados para branco
+    {
+        tamanho = this.tamanho;
+        int largura = tamanho / this.altura;
+        int altura = tamanho / this.largura;
+        int[,,] pixel = new int[altura, largura, this.rgb.Length];
+        for (int alt = 0; alt < altura; alt++)
+            for (int larg = 0; larg < largura; larg++)
+                for (int r = 0; r < this.rgb.Length; r++)
+                    this.rgb[r] = 255;   
+    }
+
+
+    
     
 }
--- 02Lista/02Lista/Program.cs
+++ 02Lista/02Lista/Program.cs
@@ -11,7 +11,20 @@ namespace _02Lista
         static void Main()
         {
             Imagem oi = new Imagem();
-            Console.WriteLine(oi.imagem);
+            
+
+            oi.SetAltura(3);
+            oi.SetLargura(3);
+            Console.WriteLine("Eis o tamanho: " + oi.CalculaTamanho());
+
+            Imagem oi2 = new Imagem(oi.largura*oi.altura);
+
+            //oi.FazImagem(oi.CalculaTamanho());
+
+            for (int alt = 0; alt < oi2.altura; alt++)
+                for (int larg = 0; larg < oi2.largura; larg++)
+                    for (int r = 0; r < oi2.rgb.Length; r++)
+                        Console.WriteLine("Altura: "+alt+", largura: "+larg+", canal de cor "+r+": "+oi2.rgb[r]);
         }
         
     }
